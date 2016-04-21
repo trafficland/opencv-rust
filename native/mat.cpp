f5ea74c459b1d73722855f5491b3a8c2a78eca9e
Ryan Bair <rbair@trafficland.com>
2016-04-21 15:08:54 -0400

    Add methods for accessing Mat contents by index for float and double values

--- native/mat.cpp
+++ native/mat.cpp
@@ -6,4 +6,8 @@ extern "C" {
   float cv_core_Mat_at_int_i_float(void* mat, int i) {
     return ((cv::Mat*)mat)->at<float>(i);
   }
+  
+  double cv_core_Mat_at_int_i_double(void* mat, int i) {
+    return ((cv::Mat*)mat)->at<double>(i);
+  }
 }
--- src/mat.rs
+++ src/mat.rs
@@ -2,12 +2,19 @@ use libc::c_void;
 
 extern "C" {
     #[doc(hidden)] pub fn cv_core_Mat_at_int_i_float(mat: *const c_void, i: i32) -> f32;
+    #[doc(hidden)] pub fn cv_core_Mat_at_int_i_double(mat: *const c_void, i: i64) -> f64;
 }
 
 impl ::core::Mat {
-    pub fn at(&self, i: i32) -> f32 {
+    pub fn at_f32(&self, i: i32) -> f32 {
         unsafe {
             cv_core_Mat_at_int_i_float(self.as_raw_Mat(), i)
+        }
+    }
+    
+    pub fn at_f64(&self, i: i64) -> f64 {
+        unsafe {
+            cv_core_Mat_at_int_i_double(self.as_raw_Mat(), i)
         }
     }
 }

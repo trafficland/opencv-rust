c50bfe5f47349eccd5a5947dd5050b42426eeafe
Ryan Bair <rbair@trafficland.com>
2016-06-28 18:02:23 -0400

    manual expose more mat methods

--- native/mat.cpp
+++ native/mat.cpp
@@ -1,13 +1,38 @@
 #include "common_opencv.h"
+#include "types.h"
 
 using namespace cv;
 
 extern "C" {
+  #include "return_types.h"
   float cv_core_Mat_at_int_i_float(void* mat, int i) {
     return ((cv::Mat*)mat)->at<float>(i);
   }
   
+  float cv_core_Mat_at_int_i_int_j_float(void* mat, int i, int j) {
+    return ((cv::Mat*)mat)->at<float>(i, j);
+  }
+  
   double cv_core_Mat_at_int_i_double(void* mat, int i) {
     return ((cv::Mat*)mat)->at<double>(i);
+  }
+  
+  struct cv_return_value_void_X cv_core_Mat_Mat_rows_cols_type_data(int rows, int cols, int type, void* data, size_t step) {
+    try {
+      cv::Mat* cpp_return_value = new cv::Mat(rows, cols, type, data, step);
+      return { NULL, (void*) cpp_return_value }; 
+    } catch (cv::Exception& e) {
+      char* msg = strdup(e.what());
+      struct cv_return_value_void_X ret;
+      memset(&ret, 0x00, sizeof(ret));
+      ret.error_msg = msg;
+      return ret;
+    } catch (...) {
+      char* msg = strdup("unspecified error in OpenCV guts");
+      struct cv_return_value_void_X ret;
+      memset(&ret, 0x00, sizeof(ret));
+      ret.error_msg = msg;
+      return ret;
+    }
   }
 }
--- src/mat.rs
+++ src/mat.rs
@@ -1,8 +1,13 @@
+use std::ffi::CStr;
 use libc::c_void;
+
+use sys::cv_return_value_void_X;
 
 extern "C" {
     #[doc(hidden)] pub fn cv_core_Mat_at_int_i_float(mat: *const c_void, i: i32) -> f32;
+    #[doc(hidden)] pub fn cv_core_Mat_at_int_i_int_j_float(mat: *const c_void, i: i32, j: i32) -> f32;
     #[doc(hidden)] pub fn cv_core_Mat_at_int_i_double(mat: *const c_void, i: i64) -> f64;
+    #[doc(hidden)] pub fn cv_core_Mat_Mat_rows_cols_type_data(rows: i32, cols: i32, dtype: i32, data: *const c_void, step: usize) -> cv_return_value_void_X;
 }
 
 impl ::core::Mat {
@@ -12,9 +17,27 @@ impl ::core::Mat {
         }
     }
     
+    pub fn at_f32_2(&self, i: i32, j: i32) -> f32 {
+        unsafe {
+            cv_core_Mat_at_int_i_int_j_float(self.as_raw_Mat(), i, j)
+        }
+    }
+    
     pub fn at_f64(&self, i: i64) -> f64 {
         unsafe {
             cv_core_Mat_at_int_i_double(self.as_raw_Mat(), i)
+        }
+    }
+    
+    pub fn from_raw_parts(rows: i32, cols: i32, dtype: i32, data: &[u8], step: usize) -> Result<::core::Mat, String> {
+        unsafe {
+            let rv = cv_core_Mat_Mat_rows_cols_type_data(rows, cols, dtype, data.as_ptr() as *const c_void, step);
+            if rv.error_msg as i32 != 0i32 {
+                let v = CStr::from_ptr(rv.error_msg).to_bytes().to_vec();
+                ::libc::free(rv.error_msg as *mut c_void);
+                return Err(String::from_utf8(v).unwrap())
+            }
+            Ok(::core::Mat{ ptr: rv.result, owned: true })
         }
     }
 }
